7cd844598d2241bbf829240dd3398506527496bf
Júlia <jfranca.contato@gmail.com>
2023-04-03 07:53:37 -0300

    explicações das variáveis

--- prog.js
+++ prog.js
@@ -1,2 +1,17 @@
-const texto = "oi"
-const pi = 3.1415
+//linguagem interpretada lê linha por linha
+    //constante => rótulo
+    const texto = "oi"
+    const pi = 3.14
+    //variável => não aceita sobrescrita
+    let frase = "meu deus, que fome" 
+    //variável => aceita sobrescrita
+    var texto2 = "teste de variável"
+    var texto2 = "computação"
+    //declaração feita pelo interpretador JS. padrão var
+    texto3 = "cinco"
+
+console.log(texto)
+console.log(pi)
+console.log(frase)
+console.log(texto2)
+console.log(texto3)
